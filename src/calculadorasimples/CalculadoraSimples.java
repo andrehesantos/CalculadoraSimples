e9304b23938bb3abeea3d52e79acbd80b9dce5e7
andrehesantos <30960536+andrehesantos@users.noreply.github.com>
2021-08-10 13:31:19 -0300

    teste

---
+++ src/calculadorasimples/CalculadoraSimples.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadorasimples;
+
+/**
+ *
+ * @author Andre Santos
+ */
+public class CalculadoraSimples {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
