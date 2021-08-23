ad0b571da379dedcdeaa6b44a7de7956f9707b0d
andrehesantos <30960536+andrehesantos@users.noreply.github.com>
2021-08-23 14:34:09 -0300

    Adição da classe CalculadoraSimples e mudanças na TelaPrincipal

--- src/calculadorasimples/CalculadoraSimples.java
+++ src/calculadorasimples/CalculadoraSimples.java
@@ -11,11 +11,6 @@ package calculadorasimples;
  */
 public class CalculadoraSimples {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+
     
 }
