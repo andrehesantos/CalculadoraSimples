7a726a26c05bbdbda9bbd7ffd5ddaef82726fbb0
andrehesantos <30960536+andrehesantos@users.noreply.github.com>
2021-08-23 22:57:19 -0300

    Adição do método multiplica

--- src/calculadorasimples/CalculadoraSimples.java
+++ src/calculadorasimples/CalculadoraSimples.java
@@ -10,7 +10,41 @@ package calculadorasimples;
  * @author Andre Santos
  */
 public class CalculadoraSimples {
+     private double operando1;
+     private double operando2;
+     private double total;
 
+    public CalculadoraSimples() {
+    }
+
+    public double getOperando1() {
+        return operando1;
+    }
+
+    public void setOperando1(double operando1) {
+        this.operando1 = operando1;
+    }
+
+    public double getOperando2() {
+        return operando2;
+    }
+
+    public void setOperando2(double operando2) {
+        this.operando2 = operando2;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+     
+    public double multiplica(double valor1, double valor2){
+        total = valor1 * valor2;
+        return total;
+    } 
 
     
 }
--- src/calculadorasimples/TelaPrincipal.java
+++ src/calculadorasimples/TelaPrincipal.java
@@ -5,17 +5,21 @@
  */
 package calculadorasimples;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Andre Santos
  */
 public class TelaPrincipal extends javax.swing.JFrame {
-
+    CalculadoraSimples calc = new CalculadoraSimples();
     /**
      * Creates new form TelaPrincipal
      */
     public TelaPrincipal() {
         initComponents();
+        
+  
     }
 
     /**
@@ -27,21 +31,134 @@ public class TelaPrincipal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        edt_operando1 = new javax.swing.JTextField();
+        edt_operando2 = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        btn_multi = new javax.swing.JButton();
+        btn_div = new javax.swing.JButton();
+        btn_soma = new javax.swing.JButton();
+        btn_sub = new javax.swing.JButton();
+        btn_limpar = new javax.swing.JButton();
+        btn_sair = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        label_result = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("Valor 1:");
+
+        jLabel2.setText("Valor 2:");
+
+        btn_multi.setText("*");
+        btn_multi.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btn_multiMouseClicked(evt);
+            }
+        });
+
+        btn_div.setText("/");
+
+        btn_soma.setText("+");
+
+        btn_sub.setText("-");
+
+        btn_limpar.setText("Limpar");
+
+        btn_sair.setText("Sair");
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 3, 18)); // NOI18N
+        jLabel3.setText("Calculadora Simples");
+
+        label_result.setFont(new java.awt.Font("Tahoma", 3, 24)); // NOI18N
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 204, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(80, 80, 80))
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(43, 43, 43)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel2))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(btn_limpar)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(btn_sair, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(edt_operando1)
+                                    .addComponent(edt_operando2, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(26, 26, 26)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(btn_multi, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(btn_div, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                        .addGap(28, 28, 28)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(btn_soma, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(btn_sub, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(168, 168, 168)
+                        .addComponent(label_result, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(68, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(68, Short.MAX_VALUE)
+                .addComponent(jLabel3)
+                .addGap(41, 41, 41)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(edt_operando1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel1))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(edt_operando2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel2)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(btn_multi)
+                            .addComponent(btn_soma))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(btn_div)
+                            .addComponent(btn_sub))))
+                .addGap(24, 24, 24)
+                .addComponent(label_result, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(28, 28, 28)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btn_sair)
+                    .addComponent(btn_limpar))
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btn_multiMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_multiMouseClicked
+        // TODO add your handling code here:
+        String edt1 = edt_operando1.getText();
+        String edt2 = edt_operando2.getText();
+        if (edt2.isEmpty() || edt2.isEmpty()){
+            JOptionPane.showMessageDialog(null, "Entre com os dois valores, por favor!");
+        }
+        else{
+           double valor1 = Double.parseDouble(edt_operando1.getText());
+           double valor2 = Double.parseDouble(edt_operando2.getText());
+           double resultado = calc.multiplica(valor1, valor2);
+           label_result.setText(String.valueOf(resultado));
+        }
+    }//GEN-LAST:event_btn_multiMouseClicked
 
     /**
      * @param args the command line arguments
@@ -79,5 +196,17 @@ public class TelaPrincipal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btn_div;
+    private javax.swing.JButton btn_limpar;
+    private javax.swing.JButton btn_multi;
+    private javax.swing.JButton btn_sair;
+    private javax.swing.JButton btn_soma;
+    private javax.swing.JButton btn_sub;
+    private javax.swing.JTextField edt_operando1;
+    private javax.swing.JTextField edt_operando2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel label_result;
     // End of variables declaration//GEN-END:variables
 }
